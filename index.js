76f90abd5148b8e8b182e3d7a4666ff2996bc896
KushRohra <kushrohra13@gmail.com>
2022-12-12 23:44:46 +0530

    added frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const path = require("path")
 
 const port = process.env.PORT || 5000
 
@@ -8,6 +9,9 @@ const app = express()
 // Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/openai", require("./routes/openaiRoutes"))
 
